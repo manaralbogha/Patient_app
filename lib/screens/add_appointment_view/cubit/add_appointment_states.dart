fca6d22bb522e54ba8a0deb0492526366094ca66
Manar Albogha <100355986+manaralbogha@users.noreply.github.com>
2023-07-20 16:37:51 +0300

    complete addApoointment screen

--- lib/screens/add_appointment_view/cubit/add_appointment_states.dart
+++ lib/screens/add_appointment_view/cubit/add_appointment_states.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:patient_app/core/models/add_appointment_model.dart';
 
 abstract class AddAppointmentStates extends Equatable {
   @override
@@ -8,3 +9,19 @@ abstract class AddAppointmentStates extends Equatable {
 class AddAppointmentInitial extends AddAppointmentStates {}
 
 class SelectDayState extends AddAppointmentStates {}
+
+class SelectTimeState extends AddAppointmentStates {}
+
+class AddAppointmentFailureState extends AddAppointmentStates {
+  final String failureMsg;
+
+  AddAppointmentFailureState({required this.failureMsg});
+}
+
+class AddAppointmentLodgingState extends AddAppointmentStates {}
+
+class AddAppointmentSuccessState extends AddAppointmentStates {
+  final AddAppointmentModel addAppointmentModel;
+
+  AddAppointmentSuccessState({required this.addAppointmentModel});
+}
