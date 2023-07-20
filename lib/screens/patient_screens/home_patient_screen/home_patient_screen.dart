f604a4bb01b214dbb5bab28b5dca734544df7635
Manar Albogha <100355986+manaralbogha@users.noreply.github.com>
2023-07-20 20:25:25 +0300

    commit

---
+++ lib/screens/patient_screens/home_patient_screen/home_patient_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePatientView extends StatelessWidget {
+  const HomePatientView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
